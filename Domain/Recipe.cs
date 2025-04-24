b091b2864ea9d6669fbe50b9a120966fe1b44f1a
Hanna Liis Heno <haheno@taltech.ee>
2025-04-24 11:14:54 +0300

    Recipe domain klass tehtud

---
+++ Domain/Recipe.cs
@@ -0,0 +1,11 @@
+﻿using RecipeMvc.Data;
+namespace RecipeMvc.Domain;
+public class Recipe(RecipeData? d) : Entity<RecipeData>(d)
+{
+    public int AuthorId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+    public float Calories { get; set; }
+    public string Tags { get; set; }
+}
