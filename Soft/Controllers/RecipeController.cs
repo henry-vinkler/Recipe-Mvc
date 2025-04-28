03747db9fd0ee4bd154e036ae62c8054b8e98de4
Hanna Liis Heno <haheno@taltech.ee>
2025-04-28 12:31:42 +0300

    RecipeController Soft tehtud

---
+++ Soft/Controllers/RecipeController.cs
@@ -0,0 +1,8 @@
+﻿using RecipeMvc.Data;
+using RecipeMvc.Domain;
+using RecipeMvc.Facade;
+using RecipeMvc.Soft.Data;
+
+namespace RecipeMvc.Soft.Controllers;
+public class RecipeController(ApplicationDbContext c)
+    : BaseController<Recipe, RecipeData, RecipeView>(c, new RecipeViewFactory(), d => new(d)) { }
