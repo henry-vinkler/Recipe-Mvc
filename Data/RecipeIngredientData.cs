849140ebd5b000333e6f6b42c2d756fdc3147642
Liisa-Maria Eendla <eendlaliisamaria@gmail.com>
2025-04-29 20:37:10 +0300

    Esialgne RecipeIngredientData klassi sisu tehtud

--- Data/RecipeIngredientData.cs
+++ Data/RecipeIngredientData.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RecipeMvc.Data
-{
-    internal class RecipeIngredientData
-    {
-    }
+namespace RecipeMvc.Data;
+
+public sealed class RecipeIngredientData : EntityData<RecipeIngredientData> {
+    public int RecipeId { get; set; }
+    public int IngredientId { get; set; }
+    public float Quantity { get; set; }
+
 }
