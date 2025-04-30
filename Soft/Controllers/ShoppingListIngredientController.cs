9b7cd4bff61c24697f116099006f1c69a9265cfa
Hanna-Liisa Värik <havari@taltech.ee>
2025-05-01 01:21:24 +0300

    ShoppingListIngredientController tehtud

---
+++ Soft/Controllers/ShoppingListIngredientController.cs
@@ -0,0 +1,9 @@
+﻿using RecipeMvc.Data;
+using RecipeMvc.Domain;
+using RecipeMvc.Facade;
+using RecipeMvc.Soft.Data;
+
+namespace RecipeMvc.Soft.Controllers;
+
+public class ShoppingListIngredientController(ApplicationDbContext c) 
+    :BaseController<ShoppingListIngredient, ShoppingListIngredientData, ShoppingListIngredientView>(c, new ShoppingListIngredientViewFactory(), d => new ShoppingListIngredient(d)) { }
