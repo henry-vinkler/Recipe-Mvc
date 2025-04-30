bce3caea46da32a21dc89ea73dc66c26f195eb47
Hanna-Liisa Värik <havari@taltech.ee>
2025-05-01 01:36:46 +0300

    ShoppingListIngredients migratsioon tehtud

---
+++ Soft/Migrations/20250430223302_AddShoppingListIngredientTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeMvc.Soft.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShoppingListIngredientTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingListIngredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ShoppingListID = table.Column<int>(type: "INTEGER", nullable: false),
+                    IngredientID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<string>(type: "TEXT", nullable: false),
+                    IsChecked = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingListIngredients", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingListIngredients");
+        }
+    }
+}
