af4d89a091a309be4b6c0fb5c7a4472344de14b5
Hanna-Liisa Värik <havari@taltech.ee>
2025-04-30 14:40:07 +0300

    Esialgne ShoppingListData tehtud

--- Data/ShoppingListIngredientData.cs
+++ Data/ShoppingListIngredientData.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace RecipeMvc.Data
 {
-    public class ShoppingListIngredientData
+    public sealed class ShoppingListIngredientData:EntityData<ShoppingListIngredientData>
     {
+        public int ShoppingListID { get; set; }
+        public int IngredientID { get; set; }
+        public string Quantity { get; set; } = string.Empty;
+        public bool IsChecked { get; set; } = false;
+
     }
 }
