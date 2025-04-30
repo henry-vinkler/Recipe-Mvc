ab68e0d67103f70d1336ff951d8028f792a11c6a
Hanna-Liisa Värik <havari@taltech.ee>
2025-04-30 16:05:49 +0300

    ShoppingListIngredient Domain klass

--- Data/ShoppingListIngredientData.cs
+++ Data/ShoppingListIngredientData.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RecipeMvc.Data
+namespace RecipeMvc.Data;
+
+public sealed class ShoppingListIngredientData : EntityData<ShoppingListIngredientData>
 {
-    internal class ShoppingListIngredientData
-    {
-    }
+    public int ShoppingListID { get; set; }
+    public int IngredientID { get; set; }
+    public string Quantity { get; set; } = string.Empty;
+    public bool IsChecked { get; set; } = false;
+
 }
+
---
+++ Domain/ShoppingListIngredient.cs
@@ -0,0 +1,11 @@
+﻿using RecipeMvc.Data;
+
+namespace RecipeMvc.Domain;
+
+public class ShoppingListIngredient(ShoppingListIngredientData? d) : Entity<ShoppingListIngredientData>(d)
+{    
+    public int ShoppingListID => Data.ShoppingListID;
+    public int IngredientID => Data.IngredientID;
+    public string Quantity => Data.Quantity;
+    public bool IsChecked => Data.IsChecked;
+}
