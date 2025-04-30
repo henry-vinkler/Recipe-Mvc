f9c460350abffd5e19666e91b29af8a4cc8b8f5e
Hanna-Liisa Värik <havari@taltech.ee>
2025-05-01 01:07:47 +0300

    ShoppingListIngredientViewFactory klass

---
+++ Facade/ShoppingListIngredientViewFactory.cs
@@ -0,0 +1,11 @@
+﻿using RecipeMvc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeMvc.Facade;
+
+public sealed class ShoppingListIngredientViewFactory : AbstractViewFactory<ShoppingListIngredientData, ShoppingListIngredientView> { }
+
