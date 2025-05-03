719d42852f31816f15f169022c6e8a34ee085937
Liisa-Maria Eendla <eendlaliisamaria@gmail.com>
2025-05-04 00:55:58 +0300

    Lisasin RecipeIngredientView, mis läheb RecipeViewiga kokku.

---
+++ Facade/RecipeIngredientView.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeMvc.Facade;
+
+[DisplayName("Recipe Ingredient")]
+public class RecipeIngredientView : EntityView {
+
+    public int RecipeId { get; set; }
+
+    [DisplayName("Ingredient")]
+    public int IngredientId { get; set; }
+
+    [DisplayName("Name")]
+    public int IngredientName { get; set; }
+
+
+    [DisplayName("Quantity")]
+    [Range(0.01, 10000)]
+    public float Quantity { get; set; }
+}
