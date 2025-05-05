a570115feaedd789203cab95004dd1e23647ee7d
Henry Vinkler <hvinkl@taltech.ee>
2025-05-05 15:23:01 +0300

    just added a package

--- Data/EntityData.cs
+++ Data/EntityData.cs
@@ -9,5 +9,3 @@ public abstract class EntityData {
 public abstract class EntityData<T> : EntityData, ICloneable<T> where T : EntityData<T> {
     public virtual T Clone() => (T) MemberwiseClone();
 }
- 
-
