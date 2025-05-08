522e6df25a70b00898049ef2b83551f4c1967305
Hanna Liis Heno <haheno@taltech.ee>
2025-05-08 10:45:31 +0300

    RecipeAuthor klass tehtud ja lisatud Recipe klassi

--- Domain/Recipe.cs
+++ Domain/Recipe.cs
@@ -3,12 +3,12 @@ namespace RecipeMvc.Domain;
 public class Recipe(RecipeData? d) : Entity<RecipeData>(d)
 {
     public int AuthorId => Data.AuthorId;
+    internal List<RecipeAuthor> RecipeAuthors = [];
+    public List<UserAccount?> Users => RecipeAuthors?
+        .Where(r => r.UserAccount is not null).Select(r => r.UserAccount).ToList() ?? [];
     public string Title => Data.Title;
     public string Description => Data.Description;
     public string ImagePath => Data.ImagePath;
     public float Calories => Data.Calories;
     public string Tags => Data.Tags;
-    /*internal List<RecipeAuthor> movieRoles = [];
-    public List<UserAccount?> Persons => movieRoles?
-        .Where(r => r.Person is not null).Select(r => r.Person).ToList() ?? [];*/
 }
