b56947ed3790e94d3e87fa30e4c8c438f7049c7b
Hanna Liis Heno <haheno@taltech.ee>
2025-05-08 11:05:30 +0300

    RecipeAuthorsController tehtud

---
+++ Soft/Controllers/RecipeAuthorsController.cs
@@ -0,0 +1,9 @@
+﻿using RecipeMvc.Data;
+using RecipeMvc.Domain;
+using RecipeMvc.Facade;
+using RecipeMvc.Soft.Data;
+
+namespace RecipeMvc.Soft.Controllers;
+
+public class RecipeAuthorsController(ApplicationDbContext c)
+    : BaseController<RecipeAuthor, RecipeAuthorData, RecipeAuthorView>(c, new RecipeAuthorViewFactory(), d => new(d)){ }
