0e50acf374df82634ad7989431337e95d81e0306
Hanna Liis Heno <haheno@taltech.ee>
2025-05-08 10:18:35 +0300

    Vaheklass RecipeAuthorData Recipe ja UserAccount jaoks data tehtud

---
+++ Data/RecipeAuthorData.cs
@@ -0,0 +1,7 @@
+﻿namespace RecipeMvc.Data;
+
+public sealed class RecipeAuthorData : EntityData<RecipeAuthorData>
+{
+    public int RecipeId { get; set; }
+    public int UserAccountId { get; set; }
+}
