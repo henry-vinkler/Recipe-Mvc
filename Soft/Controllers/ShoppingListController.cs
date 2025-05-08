d895d2077039376c84842a4ab3e9ce72a8959da5
Henry Vinkler <hvinkl@taltech.ee>
2025-05-08 12:24:08 +0300

    Example Shopping list page

---
+++ Soft/Controllers/ShoppingListController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeMvc.Soft.Controllers
+{
+    public class ShoppingListController : Controller
+    {
+        public IActionResult Index()
+        {
+            // TODO: Load favorites and added recipes from the database
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateListFromMealweek()
+        {
+            // TODO: Implement functionality for creating a shopping list from the meal week
+            return Content("Functionality to be added.");
+        }
+    }
+}
