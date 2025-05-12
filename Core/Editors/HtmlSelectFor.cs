85720d6734bcc24f1c40b7aa7efbd11f3d996e54
Hanna Liis Heno <haheno@taltech.ee>
2025-05-12 09:49:21 +0300

    Esmane Core Editors HtmlSelectFor tehtud

--- Core/Editors/HtmlSelectFor.cs
+++ Core/Editors/HtmlSelectFor.cs
@@ -4,8 +4,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using RecipeMvc.Core.Editors;
 namespace Mvc.Core.Editors;
 
-public static class HtmlSelectFor
-{
+public static class HtmlSelectFor {
     public static IHtmlContent SelectFor<TModel, TResult>(
         this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e, SelectList list) =>
         h.ForInput(e, h.DropDownListFor(e, list, new { @class = "form-control" }));
