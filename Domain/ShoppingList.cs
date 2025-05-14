2d1026d7703c8dc32298b4b57003ce6d9cf2948a
Hanna-Liisa Värik <havari@taltech.ee>
2025-05-14 11:13:41 +0300

    ShoppingList domain klass

---
+++ Domain/ShoppingList.cs
@@ -0,0 +1,10 @@
+﻿using RecipeMvc.Data;
+
+namespace RecipeMvc.Domain;
+public sealed class ShoppingList(ShoppingListData? d) : Entity<ShoppingListData>(d)
+{
+    public int UserID => Data.UserID;
+    public string Name => Data.Name;
+    public bool IsChecked => Data.IsChecked;
+    public string Notes => Data.Notes;
+}
