72a75e23d72d57c8718f893c4dd89ffe09ff4f2f
Hanna Liis Heno <haheno@taltech.ee>
2025-05-14 17:54:39 +0300

    EditRecipe ja ShowRecipe autori väli lisatud

--- Soft/Program.cs
+++ Soft/Program.cs
@@ -19,8 +19,7 @@ internal class Program {
         builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
         var app = builder.Build();
 
-        using (var scope = app.Services.CreateScope())
-        {
+        using (var scope = app.Services.CreateScope()) {
             var services = scope.ServiceProvider;
             SeedData.Initialize(services);
         }
