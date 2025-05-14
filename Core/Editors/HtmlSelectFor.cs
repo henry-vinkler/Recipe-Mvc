e5fb717ad8bc1f9acefb74daac6b5b9ed07d0ddd
Hanna Liis Heno <haheno@taltech.ee>
2025-05-14 18:06:31 +0300

    HtmlSelectFor täiendatud, et teha select2

--- Core/Editors/HtmlSelectFor.cs
+++ Core/Editors/HtmlSelectFor.cs
@@ -1,11 +1,43 @@
-﻿using System.Linq.Expressions;
-using Microsoft.AspNetCore.Html;
+﻿using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using RecipeMvc.Core.Editors;
-namespace Mvc.Core.Editors;
+using System.Linq.Expressions;
+using System.Text.Encodings.Web;
 
-public static class HtmlSelectFor {
+namespace RecipeMvc.Core.Editors;
+
+public static class HtmlSelectFor
+{
     public static IHtmlContent SelectFor<TModel, TResult>(
-        this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e, SelectList list) =>
-        h.ForInput(e, h.DropDownListFor(e, list, new { @class = "form-control" }));
+       this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e,
+        SelectList list) =>
+       h.ForInput(e, h.DropDownListFor(e, list, new { @class = "form-control" }));
+
+
+    public static IHtmlContent SelectFor<TModel, TResult>(
+       this IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e,
+        string controller)
+    {
+        var lab = h.LabelFor(e, new { @class = "control-label" });
+        //var displayName = h.DisplayNameFor(e);
+        var n = h.NameFor(e);
+        var v = h.ValueFor(e);
+        var ed = new HtmlString(
+            $"<select name=\"{n}\" " +
+            $"class=\"selectItems2 form-control\" " +
+            $"data-controller=\"{controller}\" " +
+            $"data-id=\"{v}\">" +
+            $"</select>");
+        var val = h.ValidationMessageFor(e, string.Empty, new { @class = "text-danger" });
+
+        var div = new TagBuilder("div");
+        div.AddCssClass("form-group");
+        div.InnerHtml.AppendHtml(lab);
+        div.InnerHtml.AppendHtml(ed);
+        div.InnerHtml.AppendHtml(val);
+
+        var writer = new StringWriter();
+        div.WriteTo(writer, HtmlEncoder.Default);
+
+        return new HtmlString(writer.ToString());
+    }
 }
