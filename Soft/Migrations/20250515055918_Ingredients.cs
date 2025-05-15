bfce547e8cb2e7db399c1a560164ffe52e85c642
Hanna Liis Heno <haheno@taltech.ee>
2025-05-15 09:48:21 +0300

    Migratsioon tehtud, kood töötab

---
+++ Soft/Migrations/20250515055918_Ingredients.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeMvc.Soft.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ingredients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
