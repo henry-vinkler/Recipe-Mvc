e069d2baad9a126d4108837e19261cccc0b59c64
Richard Truuse <truuse.richard@gmail.com>
2025-05-19 21:45:30 +0300

    seedDatasse lisa

--- Data/MealTabelData.cs
+++
@@ -1,8 +0,0 @@
-using RecipeMvc.Aids;
-using RecipeMvc.Data;
-
-public sealed class MealTabelData : EntityData<MealTabelData>
-{
-    public MealType MealType { get; set; }
-    public int RecipeId { get; set; }
-}
