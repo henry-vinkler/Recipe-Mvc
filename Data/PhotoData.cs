24c7d43fe72fc5e9712617363f7b5d1a479badc1
Hanna Liis Heno <haheno@taltech.ee>
2025-05-19 12:25:25 +0300

    ebavajalik PhotoData klass kustutatud

--- Data/PhotoData.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace RecipeMvc.Data;
-
-public sealed class PhotoData : EntityData<PhotoData> {
-    public int Id { get; set; } 
-    public int RecipeId { get; set; } 
-    public string FileName { get; set; } 
-    public DateTime Uploaded { get; set; }
-}
