b3ebb45dacd9e23c122b2476b1675bc4b8991f54
Liisa-Maria Eendla <eendlaliisamaria@gmail.com>
2025-06-02 01:41:06 +0300

    RecipeIngredientDataTests tehtud

---
+++ Tests/Data/RecipeIngredientDataTests.cs
@@ -0,0 +1,36 @@
+﻿using RecipeMvc.Data;
+
+namespace RecipeMvc.Tests.Data;
+
+[TestClass]
+public class RecipeIngredientDataTests : SealedTests<RecipeIngredientData, EntityData<RecipeIngredientData>> {
+
+    [TestInitialize]
+    public override void Initialize()
+    {
+        base.Initialize();
+        if (obj == null) return;
+
+        obj.Id = 1;
+        obj.RecipeId = 10;
+        obj.IngredientId = 5;
+        obj.Quantity = 2.5f;
+    }
+
+
+    [TestMethod]
+    public void RecipeIdTest() => isProperty<int>();
+
+    [TestMethod]
+    public void IngredientIdTest() => isProperty<int>();
+
+    [TestMethod]
+    public void QuantityTest() => isProperty<float>();
+
+    [TestMethod]
+    public void UnitTest() => isProperty<string>();
+
+
+
+
+}
