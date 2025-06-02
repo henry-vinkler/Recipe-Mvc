f35138a6c2e37a226dc0c0a4060a038d0a5e3428
Hanna-Liisa Värik <havari@taltech.ee>
2025-06-02 15:37:36 +0300

    ShoppingList facade testid

---
+++ Tests/Facade/ShoppingListViewFactoryTests.cs
@@ -0,0 +1,7 @@
+﻿using RecipeMvc.Facade;
+using RecipeMvc.Data;
+
+namespace RecipeMvc.Tests.Facade;
+
+[TestClass] public class ShoppingListViewFactoryTests :
+    SealedTests<ShoppingListViewFactory, AbstractViewFactory<ShoppingListData, ShoppingListView>>{ }
---
+++ Tests/Facade/ShoppingListViewTests.cs
@@ -0,0 +1,16 @@
+﻿using RecipeMvc.Facade;
+
+namespace RecipeMvc.Tests.Facade;
+
+[TestClass]
+public class ShoppingListViewTests : SealedTests<ShoppingListView, EntityView>
+{
+    [TestMethod] public override void DisplayNameTest() => isDisplayName("Shopping List");
+
+    [TestMethod] public void IdTest() => isProperty<int>();
+    [TestMethod] public void UserIdTest() => isProperty<int>();
+    [TestMethod] public void NameTest() => isProperty<string>();
+    [TestMethod] public void NotesTest() => isProperty<string>();
+    [TestMethod] public void IsCheckedTest() => isProperty<bool>();
+    [TestMethod] public void IngredientsTest() => isProperty<IList<ShoppingListIngredientView>>();
+}
